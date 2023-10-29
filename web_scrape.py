1e93eb0dfdd29a3d67531da13ccc67e1d385b4d5
Matt Vu <tmattvu@ad.unc.edu>
2023-10-29 03:16:13 -0400

    progress on web hook

--- web_scrape.py
+++ web_scrape.py
@@ -2,20 +2,41 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.by import By
+from flask import Flask, request, jsonify
 
+app = Flask(__name__)
 
-driver = webdriver.Chrome()
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    data = request.get_json()
+    query = data['queryResult']['queryText']
 
-search_url = "https://www.who.int/data/collections"
-driver.get(search_url)
+    # Set up the Selenium WebDriver (Chrome)
+    driver = webdriver.Chrome()
 
-box = driver.find_element(By.XPATH, '//*[@id="PageContent_C035_Col00"]/label/input')
-box.send_keys("tuberculosis")
-box.send_keys(Keys.RETURN)
-time.sleep(5)
-first_result_link = driver.find_element(By.CSS_SELECTOR, "#collections > div:nth-child(14) > div > a")
-first_result_url = first_result_link.get_attribute("href")
-print("First search result URL:", first_result_url)
+    # Navigate to the WHO data collections URL
+    search_url = "https://www.who.int/data/collections"
+    driver.get(search_url)
 
-driver.close()
+    # Find the search bar element and input the user's query
+    box = driver.find_element(By.XPATH, '//*[@id="PageContent_C035_Col00"]/label/input')
+    box.send_keys(query)
+    box.send_keys(Keys.RETURN)
+    time.sleep(5)
+
+    # Locate the first search result link and retrieve its URL
+    first_result_link = driver.find_element(By.CSS_SELECTOR, "#collections > div:nth-child(14) > div > a")
+    first_result_url = first_result_link.get_attribute("href")
+    
+    # Close the browser
+    driver.close()
+
+    # Create a response for Dialogflow
+    response = {
+        'fulfillmentText': f'First search result URL for {query}: {first_result_url}'
+    }
+
+    return jsonify(response)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
